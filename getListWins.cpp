35f72ba3c901e23227310a68de5cbe844778e044
ofuron72 <pereligin.3004@gmail.com>
2024-09-17 09:11:27 +0300

    display active wins

---
+++ getListWins.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "Winuser.h"
+
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
+    
+    if (IsWindowVisible(hwnd)) {
+
+        std::cout << "HWND: " << hwnd << std::endl;
+    }
+    return TRUE; 
+}
+
+
+int main()
+{
+    EnumWindows(EnumWindowsProc, NULL);
+    return  0;
+}
